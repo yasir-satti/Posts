cb355dc230ee416dbf524a4d136352b097607051
Yasir Satti <ysatti@jaguarlandrover.com>
2024-11-04 13:46:10 +0000

    first commit

---
+++ src/test/java/dev/danvega/posts/ApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.danvega.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
