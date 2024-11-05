c780a446d330179a722219513edfce986585cd50
Yasir Satti <ysatti@jaguarlandrover.com>
2024-11-05 12:40:10 +0000

    Tests for PostController methods

--- src/main/java/dev/danvega/posts/controller/PostController.java
+++ src/main/java/dev/danvega/posts/controller/PostController.java
@@ -1,12 +1,14 @@
 package dev.danvega.posts.controller;
 
 import dev.danvega.posts.data.Post;
+import dev.danvega.posts.exception.PostNotFoundException;
 import dev.danvega.posts.repository.PostRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -19,7 +21,48 @@ public class PostController {
     }
 
     @GetMapping("")
-    public List<Post> findAll(){
+    public List<Post> findAll() {
         return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Post> findById(@PathVariable Integer id) {
+        return Optional.ofNullable(
+                postRepository.findById(id)
+                        .orElseThrow(PostNotFoundException::new));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public Post create(@RequestBody @Validated Post post){
+        return postRepository.save(post);
+    }
+
+    @PutMapping("{id}")
+    public Post update(@PathVariable Integer id,  @RequestBody @Validated Post post){
+        Optional<Post> existingPost = postRepository.findById(id);
+        if(existingPost.isPresent()){
+            Post updatedPost = new Post(
+                    existingPost.get().id(),
+                    existingPost.get().userId(),
+                    post.title(),
+                    post.title(),
+                    existingPost.get().version()
+            );
+            return postRepository.save(updatedPost);
+        } else {
+            throw new PostNotFoundException();
+        }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Integer id){
+        Optional<Post> existingPost = postRepository.findById(id);
+        if(existingPost.isPresent()){
+            postRepository.deleteById(id);
+        } else {
+            throw new PostNotFoundException();
+        }
     }
 }
